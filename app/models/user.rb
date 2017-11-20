c4eb11d0339cdc35a7b116b90e633b60285a90b5
Michelle Brien <michelle.brien12@gmail.com>
2017-11-20 19:27:08 -0200

    fixed chef in form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :meals, foreign_key: 'chef_id'
   has_many :bookings, foreign_key: 'buyer_id', through: :meals
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
 end
