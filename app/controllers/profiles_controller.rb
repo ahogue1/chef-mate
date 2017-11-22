dc211fe955f2f03e5006643789bf453838bd20e7
Michelle Brien <michelle.brien12@gmail.com>
2017-11-22 13:46:14 -0200

    profiles controller updated

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,9 @@
 class ProfilesController < ApplicationController
 
   def show
-      @user = User.find(params[:id])
+    @user = User.find(params[:id])
+    @booking = Booking.where(:buyer_id == @user[:id])
+    @meal = @user.meals
   end
+
 end
