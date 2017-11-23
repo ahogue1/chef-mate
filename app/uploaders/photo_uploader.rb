5eb7822e8bf8046e715540477c1b660148a01ede
Amy Hogue <hogue.a@gmail.com>
2017-11-23 15:31:32 -0200

    make uploaded photos smaller

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -6,6 +6,14 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :standard do
+    resize_to_fit 800, 600
+  end
+
   # Choose what kind of storage to use for this uploader:
 
   # storage :fog
