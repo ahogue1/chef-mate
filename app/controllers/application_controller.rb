df487d41b2c8abee9ef962ae1173f26a95db922c
Amy Hogue <hogue.a@gmail.com>
2017-11-23 17:41:55 -0200

    added some styling to the cards and limited the size of photos for cards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :store_user_location!, if: :storable_location?
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -9,5 +10,16 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:phone_number])
   end
+
+  private
+
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+    end
+
+    def store_user_location!
+      # :user is the scope we are authenticating
+      store_location_for(:user, request.fullpath)
+    end
 end
 
--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,6 +1,6 @@
 class MealsController < ApplicationController
   before_action :set_meal, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :authenticate_user!, only: [:index, :show]
   # GET /meals
   # GET /meals.json
   def index
