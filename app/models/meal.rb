e500aa1218d011e12ce445460d2aefe8460cef1f
Gabriel Pessanha <gabrielcmp1@gmail.com>
2017-11-24 15:18:25 -0200

    finished search

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -12,11 +12,15 @@ class Meal < ApplicationRecord
     google = "https://maps.googleapis.com/maps/api/geocode/json?address="
     key = ENV['GOOGLE_API_SERVER_KEY']
     url = "#{google}#{self.address}&key=#{key}"
-    p address_hash = JSON.parse(open(url).read)["results"][0]
 
-    self.formatted_address = address_hash["formatted_address"]
-    self.latitude = address_hash["geometry"]["location"]["lat"]
-    self.longitude = address_hash["geometry"]["location"]["lng"]
-
+    if !self.address.blank?
+      address_hash = JSON.parse(open(url).read)
+      if address_hash['status'] == 'OK'
+        address_hash = address_hash["results"][0]
+        self.formatted_address = address_hash["formatted_address"]
+        self.latitude = address_hash["geometry"]["location"]["lat"]
+        self.longitude = address_hash["geometry"]["location"]["lng"]
+      end
+    end
   end
 end
